0882f36b12f4593ea38e598e77a17c167ba32126
isaacmabueno <isaacmabueno@gmail.com>
2015-08-04 09:35:39 -0500

    update

--- public/scripts/js/app.js
+++ public/scripts/js/app.js
@@ -9,18 +9,47 @@ $( document ).ready(function() {
       success: function(data) {
         console.log(data);
         for (var i = 0; i < 10; i++) {
-          $("ul").append("<li>" + data.events.event[i].title + "  |  " + data.events.event[i].venue.name + "  |  " + data.events.event[i].startDate +"</li>");
+          $("ul").append("<li>" + data.events.event[i].title + "  |  " + data.events.event[i].venue.name + "  |  " + formatDate(data.events.event[i].startDate) +"</li>");
         }
 
       }
     });
 
-    $('.forgot-pass').click(function(event) {
-      $(".pr-wrap").toggleClass("show-pass-reset");
-    });
-
-    $('.pass-reset-submit').click(function(event) {
-      $(".pr-wrap").removeClass("show-pass-reset");
-    });
-
 });
+$('.forgot-pass').click(function(event) {
+  $(".pr-wrap").toggleClass("show-pass-reset");
+});
+
+$('.pass-reset-submit').click(function(event) {
+  $(".pr-wrap").removeClass("show-pass-reset");
+});
+function formatDate(date) {
+var d = new Date(date);
+var hh = d.getHours();
+var m = d.getMinutes();
+var s = d.getSeconds();
+var dd = "AM";
+var h = hh;
+if (h >= 12) {
+    h = hh-12;
+    dd = "PM";
+}
+if (h == 0) {
+    h = 12;
+}
+m = m<10?"0"+m:m;
+
+s = s<10?"0"+s:s;
+
+/* if you want 2 digit hours:
+h = h<10?"0"+h:h; */
+
+var pattern = new RegExp("0?"+hh+":"+m+":"+s);
+
+var replacement = h+":"+m;
+/* if you want to add seconds
+replacement += ":"+s;  */
+replacement += " "+dd;
+
+return date.replace(pattern,replacement);
+}
