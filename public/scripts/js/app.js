bf238dc33cb7fb7bb5a376cd6de77b19b1d1d593
BradBolander <bradbolander@BradBolanders-MacBook-Pro.local>
2015-08-05 11:43:37 -0500

    updatesss

--- public/scripts/js/app.js
+++ public/scripts/js/app.js
@@ -19,14 +19,19 @@ $( document ).ready(function() {
 
     $.ajax({
       method: "GET",
-      url: "http://ws.audioscrobbler.com/2.0/?method=geo.getevents&location=los angeles&api_key=2e8b4c0a324101689acd8d782097b1fc&format=json",
+      url: "http://ws.audioscrobbler.com/2.0/?method=geo.getevents&location=madrid&api_key=2e8b4c0a324101689acd8d782097b1fc&format=json",
       dataType: "json",
 
       success: function(data) {
         console.log(data);
+        // var lat = new Array();
+        // var long = new Array();
         for (var i = 0; i < 10; i++) {
-          $("ul").append("<li>" + data.events.event[i].title + "  |  " + data.events.event[i].venue.name + "  |  " + formatDate(data.events.event[i].startDate) +"</li>");
-          var lat = data.events.event[i].venue.location['geo:point']['geo:lat'];
+          var event = data.events.event[i];
+          $("ul").append("<li>" + event.title + "  |  " + event.venue.name + "  |  " + formatDate(event.startDate) + "  |  " + event.venue.location['geo:point']['geo:lat'] + "  |  " + event.venue.location['geo:point']['geo:long'] + "</li>");
+          $("ul").append("<li><img height='200px' width='200px' src='"+ event.image[3]['#text'] + "'></li>");
+          // lat[i] = event.venue.location['geo:point']['geo:lat'];
+          // long[i] = event.venue.location['geo:point']['geo:long'];
 
         }
 
@@ -51,32 +56,32 @@ $( document ).ready(function() {
 
 
 function formatDate(date) {
-var d = new Date(date);
-var hh = d.getHours();
-var m = d.getMinutes();
-var s = d.getSeconds();
-var dd = "AM";
-var h = hh;
-if (h >= 12) {
-    h = hh-12;
-    dd = "PM";
-}
-if (h == 0) {
-    h = 12;
-}
-m = m<10?"0"+m:m;
+  var d = new Date(date);
+  var hh = d.getHours();
+  var m = d.getMinutes();
+  var s = d.getSeconds();
+  var dd = "AM";
+  var h = hh;
+  if (h >= 12) {
+      h = hh-12;
+      dd = "PM";
+  }
+  if (h == 0) {
+      h = 12;
+  }
+  m = m<10?"0"+m:m;
 
-s = s<10?"0"+s:s;
+  s = s<10?"0"+s:s;
 
-/* if you want 2 digit hours:
-h = h<10?"0"+h:h; */
+  /* if you want 2 digit hours:
+  h = h<10?"0"+h:h; */
 
-var pattern = new RegExp("0?"+hh+":"+m+":"+s);
+  var pattern = new RegExp("0?"+hh+":"+m+":"+s);
 
-var replacement = h+":"+m;
-/* if you want to add seconds
-replacement += ":"+s;  */
-replacement += " "+dd;
+  var replacement = h+":"+m;
+  /* if you want to add seconds
+  replacement += ":"+s;  */
+  replacement += " "+dd;
 
-return date.replace(pattern,replacement);
+  return date.replace(pattern,replacement);
 }
